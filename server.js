8ae19b74e7a0c54b549016354150bdb11660d21f
Zakaria Moshi <zkemoshi@gmail.com>
2021-05-06 09:18:39 +0300

    Express server setup with initial routes

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ app.get('/',(req,res)=> {
   res.json({msg:'Welcome to the Contact keeper API'});
 });
 
-
+//Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/auth', require('./routes/auth'));
 
 
 //Listen 
